71ab6641cde46a63466ac30b5422b5f76e4f021b
edwin <edwinwgj@hotmail.com>
2024-06-28 17:05:30 +0800

    Update sha256.lua
    change buf type

--- lib/resty/sha256.lua
+++ lib/resty/sha256.lua
@@ -32,7 +32,7 @@ int SHA256_Final(unsigned char *md, SHA256_CTX *c);
 
 local digest_len = 32
 
-local buf = ffi_new("char[?]", digest_len)
+local buf = ffi_new("unsigned char[?]", digest_len)
 local ctx_ptr_type = ffi.typeof("SHA256_CTX[1]")
 
 
